1e9309df6fe4230c01b80ae8ad632dc2f6624c75
JoanMburu <joan.wangui@student.moringaschool.com>
2024-04-21 12:52:55 +0300

    updates tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+    const taskList = document.getElementById('tasks');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const taskInput = document.getElementById('new-task-description');
+        const taskDescription = taskInput.value;
+
+        const listItem = document.createElement('li');
+        listItem.textContent = taskDescription;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent =  ' X ';
+        deleteButton.addEventListener('click', function() {
+            listItem.remove(); // Remove the task when the delete button is clicked
+        });
+
+        listItem.appendChild(deleteButton);
+
+        taskList.appendChild(listItem);
+
+        taskInput.value = '';
+    });
 });
